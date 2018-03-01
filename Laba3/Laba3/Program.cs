8c1969ba38e5d08c0943b1ba4e4c9f645fdd9e6a
sanekkurt <sanek_kurt@mail.ru>
2018-03-01 20:50:56 +0500

    first attempt

---
+++ Laba3/Laba3/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestThreeElementArray();
+            TestArrayOneHundredIdentificalNumbers();
+            TestSortArrayOf1000Elements();
+            TestSortEmptyArray();
+            Console.ReadKey();
+        }
+        private static void TestThreeElementArray()
+        {
+            //Тестирование сортировки массива из трёх элементов
+            int[] array = new[] { 3, 1, 2,};
+            int[] pass = new[] { 1, 2, 3 };
+            qSort(array);
+
+            if (array.SequenceEqual(pass)!=true)
+            {
+                Console.WriteLine("! Сортировка для массива из трех элементов работает неверно !");
+            }
+            else
+            {
+                Console.WriteLine("Сортировка для массива из трех элементов работает корректно");
+            }
+        }
+        private static void TestArrayOneHundredIdentificalNumbers()
+        {
+            //Тестирование сортировки массива из 100 одинаковых чисел
+            int[] array = new int[100];
+            int[] pass = new int[100];
+            for (int i =0; i<100;i++)
+            {
+                array[i] = 1;
+                pass[i] = 1;
+            }
+            qSort(array);
+            if (array.SequenceEqual(pass) != true)
+            {
+                Console.WriteLine("! Сортировка для массива из ста одинаковых чисел работает неверно !");
+            }
+            else
+            {
+                Console.WriteLine("Сортировка для массива из ста одинаковых чисел работает корректно");
+            }
+        }
+        private static void TestSortArrayOf1000Elements()
+        {
+            //Тестирование упорядочивания случайных пар
+            int[] array = new int[1000];
+            Random rand = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                array[i] = rand.Next(1, 1000);
+            }
+            qSort(array);
+            for(int i = 0; i<10; i++)
+            {
+                int j = rand.Next(1, 999);
+                if(array[j]<=array[j+1])
+                {
+                    Console.WriteLine("{0} случайная пара упорядочена верно", i+1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} случайная пара упорядочена не верно", i+1);
+                }
+            }
+        }
+        private static void TestSortEmptyArray()
+        {
+            //Тест на сортировку пустого массива
+            int[] array = new int[0];
+            int[] pass = new int[0];
+            qSort(array);
+            if (array.SequenceEqual(pass) != true)
+            {
+                Console.WriteLine("! Сортировка пустого массива работает неверно !");
+            }
+            else
+            {
+                Console.WriteLine("Сортировка пустого массива работает корректно");
+            }
+        }
+        static void qSort(int[] array)
+        {
+            if (array.Length == 0)//проверка на нулевой размер массива
+            {
+                int[] temp = new int[0];
+            }
+            else
+            qSort(array, 0, array.Length - 1);
+        }
+        static void qSort(int[] array, int start, int end)
+        {
+            if (end == start) return;
+            var support = array[end];
+            var upperIndex = start;
+            for (int lowerIndex = start; lowerIndex <= end - 1; lowerIndex++)
+                if (array[lowerIndex] <= support)
+                {
+                    var temp = array[lowerIndex];
+                    array[lowerIndex] = array[upperIndex];
+                    array[upperIndex] = temp;
+                    upperIndex++;
+                }
+            var n = array[upperIndex];
+            array[upperIndex] = array[end];
+            array[end] = n;
+            if (upperIndex > start) qSort(array, start, upperIndex - 1);
+            if (upperIndex < end) qSort(array, upperIndex + 1, end);
+        }
+    }
+}
---
+++ Laba3/MyTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("712182cb-5049-4faa-a99a-8a9acf9dfbaa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
