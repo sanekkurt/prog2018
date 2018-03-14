d05190b129d0a17173f617008433038ef5172805
sanekkurt <sanek_kurt@mail.ru>
2018-03-14 12:40:15 +0500

    4

---
+++ Laba4/Laba4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4
+{
+    class HashTable
+    {
+        /// <summary>
+        /// Конструктор контейнера
+        /// summary>
+        /// size">Размер хэ-таблицы
+        public HashTable(int size)
+        {
+
+        }
+        /// 
+        /// Метод складывающий пару ключь-значение в таблицу
+        /// 
+        /// key">
+        /// value">
+        public void PutPair(object key, object value)
+        {
+
+        }
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключь
+        /// <returns>Значение, null если ключ отсутствуетreturns>
+        public object GetValueByKey(object key)
+        {
+
+        }
+    }
+}
