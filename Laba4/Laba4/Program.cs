921dd5ee76819a9b718f8de97f99fb7d1c5c0818
sanekkurt <sanek_kurt@mail.ru>
2018-04-04 13:04:44 +0500

    4

--- Laba4/Laba4/Program.cs
+++ Laba4/Laba4/Program.cs
@@ -6,33 +6,67 @@ using System.Threading.Tasks;
 
 namespace Laba4
 {
-    class HashTable
+    class Program
     {
-        /// <summary>
-        /// Конструктор контейнера
-        /// summary>
-        /// size">Размер хэ-таблицы
-        public HashTable(int size)
+        static void Main()
         {
+            HashTable data = new HashTable(11);
+            data.PutPair(1, "1-Odin");
+            data.PutPair(2, "2-Dva");
+            data.PutPair(3, "3-Tri");
 
+            Console.WriteLine(data.GetValueByKey(1));
+            Console.WriteLine(data.GetValueByKey(2));
+            Console.WriteLine(data.GetValueByKey(3));
         }
-        /// 
-        /// Метод складывающий пару ключь-значение в таблицу
-        /// 
-        /// key">
-        /// value">
+    }
+    public class HashTable
+    {
+        class Data
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+        List<List<Data>> list;
+        public int size = 0;
+        public HashTable(int s)
+        {
+            size = s;
+            list = new List<List<Data>>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new List<Data>());
+            }
+        }
         public void PutPair(object key, object value)
         {
-
+            var position = (int)GetPosition(key);
+            foreach (var i in list[position])
+            {
+                if (Equals(i.Key, key))
+                {
+                    i.Value = value;
+                    return;
+                }
+            }
+            list[position].Add(new Data { Key = key, Value = value });
         }
-        /// <summary>
-        /// Поиск значения по ключу
-        /// summary>
-        /// key">Ключь
-        /// <returns>Значение, null если ключ отсутствуетreturns>
         public object GetValueByKey(object key)
         {
-
+            var position = (int)GetPosition(key);
+            foreach (var i in list[position])
+            {
+                if (Equals(i.Key, key))
+                {
+                    return i.Value;
+                }
+            }
+            return null;
+        }
+        public object GetPosition(object key)
+        {
+            var position = key.GetHashCode() % size;
+            return Math.Abs(position);
         }
     }
 }
---
+++ Laba4/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTableTests
+{
+
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        public void FindThreeElementTest()
+        {
+            var data = new Laba4.HashTable(3);
+            data.PutPair("1", "1-odin");
+            data.PutPair("2", "2-dva");
+            data.PutPair("3", "3-tri");
+            Assert.AreEqual(data.GetValueByKey("1"), "1-odin");
+            Assert.AreEqual(data.GetValueByKey("2"), "2-dva");
+            Assert.AreEqual(data.GetValueByKey("3"), "3-tri");
+        }
+
+        [TestMethod]
+        public void DifferentValueTest()
+        {
+            var data = new Laba4.HashTable(3);
+            data.PutPair("1", "1-odin");
+            data.PutPair("1", "2-dva");
+            Assert.AreEqual(data.GetValueByKey("1"), "2-dva");
+        }
+
+        [TestMethod]
+        public void FindOneElementInSetTest()
+        {
+            var data = new Laba4.HashTable(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                data.PutPair(i, i);
+            }
+            Random rand = new Random();
+            int z = rand.Next(1, 10000);
+            Assert.AreEqual(data.GetValueByKey(z), z);
+        }
+
+        [TestMethod]
+        public void FindManyEmptyElementsInSetTests()
+        {
+            var data = new Laba4.HashTable(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                data.PutPair(i, i);
+            }
+            for (int i = 10000; i < 11000; i++)
+            {
+                Assert.AreEqual(data.GetValueByKey(i), null);
+            }
+        }
+    }
+}
